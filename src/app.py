ee3906f8bba2e65b562a5b0c5441c1547a71c062
jonathanmenezesm <jonathanmenezesm@gmail.com>
2025-10-28 12:06:42 -0300

    refatorar: adicionar suporte a CORS e corrigir rotas de usuários

--- src/app.py
+++ src/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from src.controller.home import home_route
 from src.controller.user_controller import user_route
+from flask_cors import CORS
 
 #Inicialização da aplicação Flask
 App = Flask(__name__)
+
+CORS(App)
 
 # Registro de Blueprint 
 App.register_blueprint(home_route)
--- src/controller/user_controller.py
+++ src/controller/user_controller.py
@@ -6,7 +6,7 @@ user_route = Blueprint('user', __name__)
 
 '''
 Criação do Blueprint para a rota de usuários
-- /user/ (GET)- Listar usuários OK
+- /users/ (GET)- Listar usuários OK
 - /user/ (POST) - Inserir cliente no servidor 
 - /user/new (GET) - Criar novo usuário
 - /user/<user_id> (GET )- Obter usuário por ID
@@ -35,18 +35,20 @@ def inserir_usuario():
     USERS.append(novo_usuario)
     return jsonify({'ok': True, 'mensagem': 'Usuário inserido com sucesso!'})
 
+
+# CREATE
 # formulario para criar novo usuário
 @user_route.route('/new', methods=['POST'])
 def registrar_usuario():
     return render_template('registrar_usuario.html')
 
-
+# READ
 # exibir detalhes do usuário
 @user_route.route('/<int:user_id>')
 def detalhar_usuario(user_id):
     return render_template('detalhar_usuario.html')
 
-
+# UPDATE
 # formulario para editar usuário
 @user_route.route('/<int:user_id>/edit')
 def editar_usuario(user_id):
@@ -59,13 +61,13 @@ def editar_usuario(user_id):
     
     return render_template('registrar_usuario.html', user=user)
 
-
+# UPDATE
 # atualizar informações do usuário
 @user_route.route('/<int:user_id>/update', methods=['PUT'])
 def atualizar_usuario(user_id):
     pass
 
-
+# DELETE
 # deletar usuário por ID
 @user_route.route('/<int:user_id>', methods=['DELETE'])
 def deletar_usuario(user_id):
